be87d2110059a576b0ce2bde45f1ba2887df0f0b
daanforever <daan.forever@gmail.com>
2019-03-27 17:01:50 +0300

    Rails 5.2 template updates

--- rails5.2.rb
+++ rails5.2.rb
@@ -1,11 +1,15 @@
 file 'Gemfile', <<-CODE
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/\#{repo}.git" }
-# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 
-gem 'rails', '~> 5.2.0'
+# ruby '2.6.0'
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '~> 5.2.2', '>= 5.2.2.1'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
+# Use Puma as the app server
+gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
@@ -19,12 +23,21 @@ gem 'coffee-rails', '~> 4.2'
 gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.5'
+# Use Redis adapter to run Action Cable in production
+# gem 'redis', '~> 4.0'
+# Use ActiveModel has_secure_password
+# gem 'bcrypt', '~> 3.1.7'
+
+# Use ActiveStorage variant
+# gem 'mini_magick', '~> 4.8'
+
+# Use Capistrano for deployment
+# gem 'capistrano-rails', group: :development
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
 gem 'jquery-rails'                    # Provides jQuery and the jQuery-ujs driver
-gem 'puma'                            # Web server. Usage: rails s
 gem 'haml'                            # HTML Abstraction Markup Language
 gem 'simple_form'                     # Forms made easy for Rails!
 gem 'responders'                      # A set of responders modules to dry up
@@ -33,30 +46,26 @@ gem 'settingson'                      # Settings management
 
 gem 'http'                            # A fast Ruby HTTP client
 gem 'faker'                           # A library for generating fake data
-gem 'rails_semantic_logger'           # A feature rich logging framework
 
 gem 'devise'                          # Flexible authentication solution for Rails with Warden
-gem 'fie'                             # Frontend framework running over a WebSocket connection
 
 group :development do
-  # gem 'railroady'                     # Class diagram generator. Usage: rake diagram:all
-  gem 'web-console'                   # Rails Console on the Browser
-  gem 'byebug'                        # TODO: add comment or delete
-  # gem 'better_errors'                 # Better errors handler
-  # gem 'binding_of_caller'             # For better_errors
-  # gem 'meta_request'                  # For RailsPanel (chrome extention)
+  # Access an interactive console on exception pages or by calling 'console' anywhere in the code.
+  gem 'web-console', '>= 3.3.0'       # Rails Console on the Browser
   gem 'rack-mini-profiler'            # Rails profiler
-  # gem 'brakeman'                      # Security scanner. Usage: brakeman [-o file.html]
-  # gem 'bullet'                        # Query optimization # TODO need to configure
   gem 'annotate'                      # Annotate ActiveRecord models. Usage: annotate
   gem 'haml-rails'                    # Integration for HAML
+  gem 'rubocop', require: false       # A Ruby static code analyzer and formatter
 end
 
 group :development, :test do
   gem 'listen', '>= 3.0.5', '< 3.2'
+  # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
+  gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
-  gem 'spring', require: false        # Spring speeds up development
   gem 'spring-commands-rspec'         # Implements the rspec command for Spring
+  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
 end
 
 group :test do
@@ -65,8 +74,10 @@ group :test do
   gem 'database_cleaner'              # Helper gem for rspec
   gem 'simplecov', require: false     # Code coverage
   gem 'webmock', require: false       # Library for stubbing HTTP requests
-  # gem 'capybara', require: false      # Acceptance test framework for web applications
 end
+
+# Windows does not include zoneinfo files, so bundle the tzinfo-data gem
+gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 CODE
 
 after_bundle do
