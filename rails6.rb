ff1203317b741cbba3a0146437cfd32319583739
dan <daan.forever@gmail.com>
2021-07-15 11:22:18 +0300

    turbolinks removed

--- rails6.rb
+++ rails6.rb
@@ -34,6 +34,7 @@ gem_group :test do
 end
 
 run "sed -i -e '/webpacker/d' Gemfile"
+run "sed -i -e '/turbolinks/d' Gemfile"
 
 after_bundle do
 
@@ -118,10 +119,9 @@ inside('app/views/layouts') do
     %meta{content: 'IE=edge', 'http-equiv' => 'X-UA-Compatible'}
     %meta{content: 'width=device-width, initial-scale=1, shrink-to-fit=no', name: 'viewport'}
     %link{rel: "stylesheet", href: "https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css", integrity: "sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB", crossorigin: "anonymous"}
-    = stylesheet_link_tag 'application', media: 'all', 'data-turbolinks-track' => true
+    = stylesheet_link_tag 'application', media: 'all'
     - unless controller.class.module_parent == Devise
-      = stylesheet_link_tag controller_path, media: 'all', 'data-turbolinks-track' => true
-    = javascript_include_tag 'turbolinks', 'data-turbolinks-track' => 'reload'
+      = stylesheet_link_tag controller_path, media: 'all'
     = csrf_meta_tags
   %body
     .container-fluid
@@ -135,9 +135,9 @@ inside('app/views/layouts') do
     %script{src: "https://code.jquery.com/jquery-3.3.1.slim.min.js", integrity: "sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo", crossorigin: "anonymous"}
     %script{src: "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js", integrity: "sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49", crossorigin: "anonymous"}
     %script{src: "https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js", integrity: "sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T", crossorigin: "anonymous"}
-    = javascript_include_tag 'application', 'data-turbolinks-track' => 'reload'
+    = javascript_include_tag 'application'
     - unless controller.class.module_parent == Devise
-      = javascript_include_tag controller_path, 'data-turbolinks-track' => 'reload'
+      = javascript_include_tag controller_path
 CODE
 
   file '_navbar.html.haml', <<-CODE
